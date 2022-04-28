0130441413853ded327257c609737b4c23738abd
faha1999 <kafaha1999@gmail.com>
2022-04-29 01:33:19 +0600

    tested data restructure

--- src/components/Chiefs.js
+++ src/components/Chiefs.js
@@ -1,54 +1,16 @@
 import React from 'react';
 import { ChiefsCard } from './ChiefsCard';
+import chiefsData from './data/chiefsData';
 
 export const Chiefs = () => {
-  const chiefsItem = [
-    {
-      name: 'Juan Carlos',
-      img: '../images/top-chiefs/img_1.jpg',
-      recipesCount: '10',
-      cuisine: 'Mexican',
-    },
-    {
-      name: 'John Doe',
-      img: '../images/top-chiefs/img_2.jpg',
-      recipesCount: '05',
-      cuisine: 'Japanese',
-    },
-    {
-      name: 'Erich Maria',
-      img: '../images/top-chiefs/img_3.jpg',
-      recipesCount: '13',
-      cuisine: 'Italian',
-    },
-    {
-      name: 'Chris Brown',
-      img: '../images/top-chiefs/img_4.jpg',
-      recipesCount: '08',
-      cuisine: 'American',
-    },
-    {
-      name: 'Blake Lively',
-      img: '../images/top-chiefs/img_5.jpg',
-      recipesCount: '09',
-      cuisine: 'French',
-    },
-    {
-      name: 'Ben Affleck',
-      img: '../images/top-chiefs/img_6.jpg',
-      recipesCount: '04',
-      cuisine: 'Indian',
-    },
-  ];
+  const chiefsItem = chiefsData.map((chiefsItem) => (
+    <ChiefsCard key={chiefsItem.name} chiefsItem={chiefsItem} />
+  ));
 
   return (
     <div className="section chiefs">
       <h1 className="title">Our Top Chiefs</h1>
-      <div className="top-chiefs-container">
-        {chiefsItem.map((chiefsItem) => (
-          <ChiefsCard key={chiefsItem.name} chiefsItem={chiefsItem} />
-        ))}
-      </div>
+      <div className="top-chiefs-container">{chiefsItem}</div>
     </div>
   );
 };
---
+++ src/components/data/chiefsData.js
@@ -0,0 +1,40 @@
+const chiefsData = [
+  {
+    name: 'Juan Carlos',
+    img: '../images/top-chiefs/img_1.jpg',
+    recipesCount: '10',
+    cuisine: 'Mexican',
+  },
+  {
+    name: 'John Doe',
+    img: '../images/top-chiefs/img_2.jpg',
+    recipesCount: '05',
+    cuisine: 'Japanese',
+  },
+  {
+    name: 'Erich Maria',
+    img: '../images/top-chiefs/img_3.jpg',
+    recipesCount: '13',
+    cuisine: 'Italian',
+  },
+  {
+    name: 'Chris Brown',
+    img: '../images/top-chiefs/img_4.jpg',
+    recipesCount: '08',
+    cuisine: 'American',
+  },
+  {
+    name: 'Blake Lively',
+    img: '../images/top-chiefs/img_5.jpg',
+    recipesCount: '09',
+    cuisine: 'French',
+  },
+  {
+    name: 'Ben Affleck',
+    img: '../images/top-chiefs/img_6.jpg',
+    recipesCount: '04',
+    cuisine: 'Indian',
+  },
+];
+
+export default chiefsData;
